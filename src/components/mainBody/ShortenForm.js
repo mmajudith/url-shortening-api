848b22fc7dda6e6506140ea0a624f82a95534ada
mma judith <mmajudith2000@gmail.com>
2021-02-26 20:28:05 -0800

    URL Shorten api

--- src/components/mainBody/ShortenForm.js
+++ src/components/mainBody/ShortenForm.js
@@ -1,12 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function ShortenForm() {
+
+  const [input, setInput] = useState("")
+
+  const formSubmit = e =>{
+      e.preventDefault()
+
+      const regx = /^[\w.]+@\w+\.(com|net|org|oi)$/;
+
+      // if (){
+
+      // }else{
+
+      // }
+      // console.log(input)
+
+
+      setInput("")
+  }
+
+  // https://api.shrtco.de/v2/
+  // shorten
+  // GET/POST: https://api.shrtco.de/v2/shorten?url=example.org/very/long/link.html
+
   return (
     <div className="form-wrapper">
-      <form>
+      <form onSubmit={formSubmit} method="GET">
         <div className="inputBut-wrapper">
           <div className="input-wrapper">
-            <input type="text" placeholder="shorten a link here..."/>
+            <input type="text"
+             placeholder="shorten a link here..." 
+             value={input}
+             onChange={e => setInput(e.target.value)}/>
             <p>please add a link</p>
           </div>
           <div className="button-wrapper">
